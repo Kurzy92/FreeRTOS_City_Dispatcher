b4dc377be1c37ba85b336e4e9c924c272ad00fd2
Eyal Kurz <eyalkurz@gmail.com>
2024-07-16 23:53:03 +0300

    a

--- Drivers/lv_conf.h
+++ Drivers/lv_conf.h
@@ -12,7 +12,7 @@
  */
 
 /* clang-format off */
-#if 0 /*Set it to "1" to enable content*/
+#if 1 /*Set it to "1" to enable content*/
 
 #ifndef LV_CONF_H
 #define LV_CONF_H
@@ -995,7 +995,7 @@
 #define LV_USE_ST7735        0
 #define LV_USE_ST7789        0
 #define LV_USE_ST7796        0
-#define LV_USE_ILI9341       0
+#define LV_USE_ILI9341       1
 
 #define LV_USE_GENERIC_MIPI (LV_USE_ST7735 | LV_USE_ST7789 | LV_USE_ST7796 | LV_USE_ILI9341)
 
